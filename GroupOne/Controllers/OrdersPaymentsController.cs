6991bbb28be357ef278ce9d5cf4a6285bee2ac00
Abdullahi <tb2332wz@go.minnstate.edu>
2022-04-08 20:00:04 -0500

    initial commit

---
+++ GroupOne/Controllers/OrdersPaymentsController.cs
@@ -0,0 +1,109 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GroupOne.Data;
+using GroupOne.Models;
+
+namespace GroupOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersPaymentsController : ControllerBase
+    {
+        private readonly Group1ComputerStore4Context _context;
+
+        public OrdersPaymentsController(Group1ComputerStore4Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/OrdersPayments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrdersPayment>>> GetOrdersPayment()
+        {
+            return await _context.OrdersPayment.ToListAsync();
+        }
+
+        // GET: api/OrdersPayments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrdersPayment>> GetOrdersPayment(int id)
+        {
+            var ordersPayment = await _context.OrdersPayment.FindAsync(id);
+
+            if (ordersPayment == null)
+            {
+                return NotFound();
+            }
+
+            return ordersPayment;
+        }
+
+        // PUT: api/OrdersPayments/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrdersPayment(int id, OrdersPayment ordersPayment)
+        {
+            if (id != ordersPayment.OrderPaymentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(ordersPayment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrdersPaymentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/OrdersPayments
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<OrdersPayment>> PostOrdersPayment(OrdersPayment ordersPayment)
+        {
+            _context.OrdersPayment.Add(ordersPayment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrdersPayment", new { id = ordersPayment.OrderPaymentId }, ordersPayment);
+        }
+
+        // DELETE: api/OrdersPayments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrdersPayment(int id)
+        {
+            var ordersPayment = await _context.OrdersPayment.FindAsync(id);
+            if (ordersPayment == null)
+            {
+                return NotFound();
+            }
+
+            _context.OrdersPayment.Remove(ordersPayment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrdersPaymentExists(int id)
+        {
+            return _context.OrdersPayment.Any(e => e.OrderPaymentId == id);
+        }
+    }
+}
---
+++ GroupOne/Controllers/OrdersProductsController.cs
@@ -0,0 +1,109 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GroupOne.Data;
+using GroupOne.Models;
+
+namespace GroupOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersProductsController : ControllerBase
+    {
+        private readonly Group1ComputerStore4Context _context;
+
+        public OrdersProductsController(Group1ComputerStore4Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/OrdersProducts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrdersProduct>>> GetOrdersProduct()
+        {
+            return await _context.OrdersProduct.ToListAsync();
+        }
+
+        // GET: api/OrdersProducts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrdersProduct>> GetOrdersProduct(int id)
+        {
+            var ordersProduct = await _context.OrdersProduct.FindAsync(id);
+
+            if (ordersProduct == null)
+            {
+                return NotFound();
+            }
+
+            return ordersProduct;
+        }
+
+        // PUT: api/OrdersProducts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrdersProduct(int id, OrdersProduct ordersProduct)
+        {
+            if (id != ordersProduct.OrderProductId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(ordersProduct).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrdersProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/OrdersProducts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<OrdersProduct>> PostOrdersProduct(OrdersProduct ordersProduct)
+        {
+            _context.OrdersProduct.Add(ordersProduct);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrdersProduct", new { id = ordersProduct.OrderProductId }, ordersProduct);
+        }
+
+        // DELETE: api/OrdersProducts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrdersProduct(int id)
+        {
+            var ordersProduct = await _context.OrdersProduct.FindAsync(id);
+            if (ordersProduct == null)
+            {
+                return NotFound();
+            }
+
+            _context.OrdersProduct.Remove(ordersProduct);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrdersProductExists(int id)
+        {
+            return _context.OrdersProduct.Any(e => e.OrderProductId == id);
+        }
+    }
+}
